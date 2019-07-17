761a7f34c4fc782565a2765812630109eea842da
stmrocket <shawn@ibnerd.net>
2019-07-17 18:46:48 -0500

    Manually Add UniFi and NMAP Components

---
+++ custom_components/unifi/const.py
@@ -0,0 +1,15 @@
+"""Constants for the UniFi component."""
+import logging
+
+LOGGER = logging.getLogger(__package__)
+DOMAIN = 'unifi'
+
+CONTROLLER_ID = '{host}-{site}'
+
+CONF_CONTROLLER = 'controller'
+CONF_SITE_ID = 'site'
+
+UNIFI_CONFIG = 'unifi_config'
+
+CONF_DETECTION_TIME = 'detection_time'
+CONF_SSID_FILTER = 'ssid_filter'
